ea28dc36b0ccb3f9ec24d0cb778f64551010b1c5
TromboneScott <TromboneScott@yahoo.com>
2022-04-21 09:42:42 -0600

    Catch exception when unable to connect to URL

--- src/oscars/Ballots.java
+++ src/oscars/Ballots.java
@@ -25,10 +25,10 @@ public final class Ballots {
     public static void main(String[] inArgs) throws Exception {
         if (inArgs.length != 1)
             throw new IllegalArgumentException("Usage: Batch <URL>");
-        for (int entryCount = -1;;) {
-            URL url = new URL(inArgs[0]);
-            url.openConnection().setDefaultUseCaches(false);
+        for (int entryCount = -1;; Thread.sleep(10000))
             try {
+                URL url = new URL(inArgs[0]);
+                url.openConnection().setDefaultUseCaches(false);
                 Ballots ballots = new Ballots(url);
                 if (ballots.all.size() > entryCount) {
                     entryCount = ballots.all.size();
@@ -39,8 +39,6 @@ public final class Ballots {
             } catch (IOException e) {
                 System.err.println(LocalDateTime.now() + " - Error downloading entries: " + e);
             }
-            Thread.sleep(10000);
-        }
     }
 
     public Ballots(URL inURL) throws IOException, CsvException {
