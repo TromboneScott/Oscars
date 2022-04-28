b5e159103cf689e5acd71076e7561753bc91fac0
TromboneScott <TromboneScott@yahoo.com>
2022-04-28 12:53:46 -0600

    Fixed Ballot.main so it will always write results the first time

--- src/oscars/Ballot.java
+++ src/oscars/Ballot.java
@@ -22,40 +22,31 @@ import com.opencsv.exceptions.CsvException;
 public final class Ballot {
     public static final Collector<Ballot, ?, Collection<Ballot>> LATEST = Collectors
             .collectingAndThen(
-                    Collectors.toMap(ballot -> ballot.name().toUpperCase(), ballot -> ballot,
-                            BinaryOperator.maxBy(Comparator.comparing(Ballot::timestamp))),
+                    Collectors.toMap(ballot -> ballot.getName().toUpperCase(), ballot -> ballot,
+                            BinaryOperator.maxBy(Comparator.comparing(Ballot::getTimestamp))),
                     Map::values);
 
     private final String[] values;
 
-    private Ballot(String[] inValues) {
-        values = inValues;
-    }
-
     public static void main(String[] inArgs) throws Exception {
         Oscars.validateArgs(inArgs);
-        for (LocalDateTime lastTimestamp = LocalDateTime.MIN;; Thread.sleep(10000))
+        for (LocalDateTime lastTimestamp = null;; Thread.sleep(10000))
             try {
                 URL url = new URL(inArgs[0]);
                 url.openConnection().setDefaultUseCaches(false);
                 Collection<Ballot> ballots = stream(url).collect(LATEST);
-                if (ballots.stream().map(Ballot::timestamp).anyMatch(lastTimestamp::isBefore)) {
+                LocalDateTime maxTimestamp = ballots.stream().map(Ballot::getTimestamp)
+                        .max(LocalDateTime::compareTo).orElse(LocalDateTime.MIN);
+                if (lastTimestamp == null || lastTimestamp.isBefore(maxTimestamp)) {
+                    lastTimestamp = maxTimestamp;
                     Results.write(LocalDateTime.now(), ballots.stream().map(Ballot::toDOM)
                             .reduce(new Element("ballots"), Element::addContent));
                     System.err.println(
                             LocalDateTime.now() + " - Wrote " + ballots.size() + " ballots");
-                    lastTimestamp = ballots.stream().map(Ballot::timestamp)
-                            .max(LocalDateTime::compareTo).get();
                 }
             } catch (IOException e) {
                 System.err.println(LocalDateTime.now() + " - Error downloading ballots: " + e);
             }
-    }
-
-    private Element toDOM() {
-        return new Element("ballot").addContent(new Element("name").addContent(name())).addContent(
-                new Element("timestamp").setAttribute("raw", timestamp().toString()).addContent(
-                        timestamp().format(DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a"))));
     }
 
     public static Stream<Ballot> stream(URL inURL) throws IOException, CsvException {
@@ -65,19 +56,30 @@ public final class Ballot {
         }
     }
 
+    private Ballot(String[] inValues) {
+        values = inValues;
+    }
+
     public String get(int inColumn) {
         return values[inColumn];
     }
 
-    public String name() {
+    public String getName() {
         return values[2] + ", " + values[1];
     }
 
-    public String email() {
+    public String getEmail() {
         return values[values.length - 1];
     }
 
-    private LocalDateTime timestamp() {
+    private LocalDateTime getTimestamp() {
         return LocalDateTime.parse(values[0], DateTimeFormatter.ofPattern("M/d/yyyy H:mm:ss"));
+    }
+
+    private Element toDOM() {
+        return new Element("ballot").addContent(new Element("name").addContent(getName()))
+                .addContent(new Element("timestamp").setAttribute("raw", getTimestamp().toString())
+                        .addContent(getTimestamp()
+                                .format(DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm:ss a"))));
     }
 }
--- src/oscars/Emails.java
+++ src/oscars/Emails.java
@@ -5,7 +5,7 @@ import java.net.URL;
 public class Emails {
     public static void main(String[] inArgs) throws Exception {
         Oscars.validateArgs(inArgs);
-        Ballot.stream(new URL(inArgs[0])).filter(ballot -> !ballot.email().isEmpty())
-                .forEach(ballot -> System.out.println(ballot.name() + " = " + ballot.email()));
+        Ballot.stream(new URL(inArgs[0])).filter(ballot -> !ballot.getEmail().isEmpty()).forEach(
+                ballot -> System.out.println(ballot.getName() + " = " + ballot.getEmail()));
     }
 }
