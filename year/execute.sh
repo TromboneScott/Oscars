e14b4e2fd29fe4e89a3f68f1729b89f46f4fa51e
TromboneScott <TromboneScott@yahoo.com>
2023-01-27 14:35:38 -0700

    Clean up

--- year/execute.sh
+++ year/execute.sh
@@ -1,2 +1,2 @@
 export OSCARS_HOME=../../../../Java/Oscars
-java -classpath "$OSCARS_HOME;$OSCARS_HOME/bin;$OSCARS_HOME/lib/*" oscars.$1 `cat $OSCARS_HOME/ResponsesURL.txt`
+java -classpath "$OSCARS_HOME;$OSCARS_HOME/bin;$OSCARS_HOME/lib/*" oscars.$1
