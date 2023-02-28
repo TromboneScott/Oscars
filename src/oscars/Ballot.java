f868a7eb1948f850510f28e6611ee45c834b5830
TromboneScott <TromboneScott@yahoo.com>
2023-02-27 22:21:17 -0700

    Updated ballot logs

--- src/oscars/Ballot.java
+++ src/oscars/Ballot.java
@@ -5,6 +5,7 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
@@ -63,9 +64,11 @@ public final class Ballot {
             LocalDateTime maxTimestamp = ballots.stream().map(Ballot::getTimestamp)
                     .max(LocalDateTime::compareTo).orElse(LocalDateTime.MIN);
             if (inLastTimestamp == null || inLastTimestamp.isBefore(maxTimestamp)) {
+                System.err.println(LocalDateTime.now() + " - Downloaded: " + ballots.size()
+                        + " ballots - After: " + Duration.between(maxTimestamp, LocalDateTime.now())
+                                .toString().substring(2));
                 Results.write(ZonedDateTime.now(), ballots.stream().map(Ballot::toDOM)
                         .reduce(new Element("ballots"), Element::addContent));
-                System.err.println(LocalDateTime.now() + " - Wrote " + ballots.size() + " ballots");
                 return maxTimestamp;
             }
         } catch (IOException e) {
