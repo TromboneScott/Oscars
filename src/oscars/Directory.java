4e05fa037621fc00e2755cee711709b9673f2a36
TromboneScott <TromboneScott@yahoo.com>
2025-02-03 15:03:02 -0700

    Cleaned up Directory.write()

--- src/oscars/Directory.java
+++ src/oscars/Directory.java
@@ -55,13 +55,13 @@ public final class Directory extends File {
 
     /** Write an XML file in this directory with the given element, DTD and (optional) XSL file */
     public void write(String inXMLFile, String inName, Element inElement) throws IOException {
-        Document document = new Document(inElement,
-                new DocType(inName, "../dtd/" + inName + ".dtd"));
-        document.addContent(0, new Comment("OSCARS website created by Scott McDonald"));
-        document.addContent(0, new ProcessingInstruction("xml-stylesheet",
-                ImmutableMap.of("type", "text/xsl", "href", "../xsl/" + inName + ".xsl")));
         try (Writer writer = new PrintWriter(file(inXMLFile), StandardCharsets.UTF_8.name())) {
-            new XMLOutputter(Format.getPrettyFormat()).output(document, writer);
+            new XMLOutputter(Format.getPrettyFormat()).output(
+                    new Document(inElement, new DocType(inName, "../dtd/" + inName + ".dtd"))
+                            .addContent(0, new Comment("OSCARS website created by Scott McDonald"))
+                            .addContent(0, new ProcessingInstruction("xml-stylesheet", ImmutableMap
+                                    .of("type", "text/xsl", "href", "../xsl/" + inName + ".xsl"))),
+                    writer);
         }
     }
 }
